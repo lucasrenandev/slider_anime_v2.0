305b68274aece712e0684c2baf3d44863c44d5de
lucasrenandns <lucasrenandns96@gmail.com>
2022-12-24 15:33:14 -0300

    Slider

---
+++ assets/js/slider.js
@@ -0,0 +1,47 @@
+// Elementos
+const nextButton = document.querySelector(".next-button")
+const previousButton = document.querySelector(".previous-button")
+const sliderImage = document.querySelectorAll(".slide-image")
+const randomButton = document.querySelector(".random-button")
+
+// Slide atual
+let currentSlide = 0
+
+// Próximo slider
+function nextSlide() {
+    sliderImage[currentSlide].classList.remove("active")
+    currentSlide = (currentSlide + 1) % sliderImage.length
+    sliderImage[currentSlide].classList.add("active")
+}
+
+// Slide anterior
+function previousSlide() {
+    sliderImage[currentSlide].classList.remove("active")
+    currentSlide = (currentSlide - 1 + sliderImage.length) % sliderImage.length
+    sliderImage[currentSlide].classList.add("active")
+}
+
+// Remover slider ativo
+function hideSlide() {
+    sliderImage.forEach(image => image.classList.remove("active"))
+}
+
+// Adicionar slider ativo
+function showSlide() {
+    sliderImage[currentSlide].classList.add("active")
+}
+
+// Slide alatório
+function randomSlide() {
+    currentSlide = Math.floor(Math.random() * sliderImage.length)
+    hideSlide()
+    showSlide()
+}
+
+// Slide automático
+setInterval(nextSlide, 5000)
+
+// Adicionar evento aos elementos
+nextButton.addEventListener("click", nextSlide)
+previousButton.addEventListener("click", previousSlide)
+randomButton.addEventListener("click", randomSlide)
